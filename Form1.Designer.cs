951dc3c14097375412a7414b6a685372fbc312e4
Thomas S.  Knutsen <la3pna@gmail.com>
2013-12-11 09:03:47 +0100

    test code and outlines added

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace NFA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SmithPictureBox = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SmithPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.settingsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1126, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // SmithPictureBox
+            // 
+            this.SmithPictureBox.Location = new System.Drawing.Point(23, 26);
+            this.SmithPictureBox.Name = "SmithPictureBox";
+            this.SmithPictureBox.Size = new System.Drawing.Size(554, 554);
+            this.SmithPictureBox.TabIndex = 1;
+            this.SmithPictureBox.TabStop = false;
+            this.SmithPictureBox.Click += new System.EventHandler(this.SmithPictureBox_Click);
+            this.SmithPictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.SmithPictureBox_Paint);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox1.Location = new System.Drawing.Point(23, 586);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(554, 108);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TabStop = false;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1126, 704);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.SmithPictureBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SmithPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.PictureBox SmithPictureBox;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Diagnostics;   // for Debug
+using System.Numerics;  // for Complex
+
+namespace NFA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.SmithPictureBox.Invalidate();
+            Textboxadd( "Program initiated, Smith chart drawn.");
+        
+        }
+
+        private void Textboxadd(string s)
+        {
+            textBox1.Text = textBox1.Text + System.Environment.NewLine + s;
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+        }
+
+        // Create an array that defines the values of the R' and X' circles we want to display
+        float[] circles = new float[] {0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1.0f, 1.2f,
+                                             1.4f, 1.6f, 1.8f, 2.0f, 3.0f, 4.0f, 5.0f, 10.0f, 20.0f};
+
+        // Create array of magic numbers to prevent drawing arc outside the unity circle - this should be done in code!
+        float[] Magic = new float[] { 11.4f, 22.6f, 33.5f, 43.5f, 53.2f, 62, 70, 77, 84, 90, 100, 109, 116, 122, 127, 143, 152, 158, 170, 179 };
+        // Create array of magic numbers that position the X' text - this should be done in code!
+        float[] Magic_2 = new float[] { 0.0f, 0.02f, 0.07f, 0.12f, 0.20f, 0.26f, 0.34f, 0.4f, 0.46f, 0.51f, 0.6f, 0.68f, 0.73f, 0.78f, 0.82f, 0.936f, 0.97f, 0.99f, 1.02f, 1.025f };
+        float[] Magic_3 = new float[] { 0.41f, 0.31f, 0.23f, 0.16f, 0.09f, 0.05f, 0.02f, 0.0f, -0.01f, -0.01f, 0.0f, 0.02f, 0.04f, 0.07f, 0.096f, 0.2f, 0.27f, 0.314f, 0.41f, 0.46f };
+
+        private int unity_circle_dia = 0;
+
+        Pen p = new Pen(Brushes.Black);
+
+        private Form1 form;
+        public void SetForm(Form1 myForm)
+        {
+            form = myForm;
+        }
+
+        private void DrawSmithChart(Graphics g)
+        {
+            // former parameters origin_x and origin_y become 0, as they are the upper left bound of the picture box.
+            // likewise frame_size_x is 'Width' and frame_size_y is 'Height'
+            // so this means origin_unity_x and origin_unity_y are also 0
+
+
+            // Pen p = new Pen(Brushes.Black); 
+
+            Rectangle r = new Rectangle(0, 0, SmithPictureBox.Width, SmithPictureBox.Height);  // outline round chart
+            g.DrawRectangle(p, r);                // draw outline
+            // fill chart rectangle with white
+            g.FillRectangle(Brushes.White, r);
+            
+
+            // set the origin of the rectangle that contains the unity circle to be 2 * origin_x and origin_y for now
+            int origin_unity_x = offsetX;
+            int origin_unity_y = offsetY;
+
+            // Draw unity circle
+            Rectangle r_unity = new Rectangle(origin_unity_x, origin_unity_y, unity_circle_dia, unity_circle_dia);
+            g.DrawEllipse(p, r_unity);
+
+            // save x location of RHS  of unity circle
+            int unity_RHS_x = r_unity.Right;
+
+            // save y location of RHS of unity circle
+            int unity_RHS_y = (r_unity.Height / 2 + origin_unity_x);
+
+            // save unity circle radius
+            int unity_circle_radius = unity_circle_dia / 2;
+
+            // Draw pure Resistance line
+            g.DrawLine(p, r_unity.Left, unity_RHS_y, r_unity.Right, unity_RHS_y);
+
+
+            // Draw Real and Imaginary circles
+            float[] diameter_R = new float[21];
+            float[] diameter_X = new float[21];
+
+
+            // calculate diameter of R' and X' circles
+            for (int x = 0; x < circles.Length; x++)
+            {
+                diameter_R[x] = 1.0f / (1.0f + circles[x]);
+                diameter_X[x] = (float)Math.Pow(1.0f / (float)Math.Sqrt(circles[x]), 2);   // (X'^-0.5)^2
+            }
+
+            Pen p1 = new Pen(Color.Black);
+            Pen p2 = new Pen(Color.Red);
+
+            // draw  Resistive and Reactive circles and text
+            for (int x = 0; x < 20; x++)
+            {
+                Rectangle r1 = new Rectangle(unity_RHS_x - (int)(unity_circle_dia * diameter_R[x]), unity_RHS_y - (int)(unity_circle_radius * diameter_R[x]),
+                                            (int)(unity_circle_dia * diameter_R[x]), (int)(unity_circle_dia * diameter_R[x]));
+                g.DrawEllipse(p1, r1);
+
+
+                Rectangle r2 = new Rectangle(unity_RHS_x - (int)(unity_circle_dia * diameter_X[x] / 2), unity_RHS_y - (int)(unity_circle_dia * diameter_X[x]),
+                                             (int)(unity_circle_dia * diameter_X[x]), (int)(unity_circle_dia * diameter_X[x]));
+
+                g.DrawArc(p2, r2, 90, Magic[x]);
+
+                // Draw mirror image X', move rectangle down by diameter of unity circle, correct angles and draw
+                Rectangle r3 = new Rectangle(unity_RHS_x - (int)(unity_circle_dia * diameter_X[x] / 2), unity_RHS_y, (int)(unity_circle_dia * diameter_X[x]),
+                                             (int)(unity_circle_dia * diameter_X[x]));
+
+                g.DrawArc(p2, r3, 270 - Magic[x], Magic[x]);
+
+                // display R' & X' text
+                display_text(g, MyFont, r_unity, r1, circles[x], Magic_2[x], Magic_3[x]);
+            }
+        }
+
+        private void display_text(Graphics g, Font font, Rectangle r_unity, Rectangle r1, float f_text, float x, float y)
+        {
+            // Convert value to display to a string
+            string text = f_text.ToString();  // use ToString("Nx")  where x is number of decimal places to display
+
+            Font tempFont = new Font("Calibri", 12);
+
+            // display R' values
+
+            SizeF sz = g.VisibleClipBounds.Size;
+            //Offset the coordinate system so that point (0, 0) is at the top left hand corner of the rectangle
+            g.TranslateTransform(r1.Left, r1.Top);
+            //Rotate the Graphics object so that the R text reads from bottom to top
+            g.RotateTransform(270);
+            sz = g.MeasureString(text, tempFont);
+            //Offset the Drawstring method so that the center of the string is on the center line of the rectangle
+            g.DrawString(text, font, Brushes.Black, -((sz.Width / 2) + r1.Width / 2), -(sz.Height / 2));
+            //Reset the graphics object Transformations.
+            g.ResetTransform();
+
+            // display X' values above the Resistance line
+            g.TranslateTransform(r_unity.Left, r_unity.Top);
+
+            g.DrawString(text, font, Brushes.Black, -(sz.Width / 2 - (r_unity.Width * x)), -(sz.Height / 2 - (r_unity.Width * y)));
+
+            float delta = (r_unity.Width / 2) - (r_unity.Width * y);
+
+            // display X' values below the Resistance line
+            g.DrawString(text, font, Brushes.Black, -(sz.Width / 2 - (r_unity.Width * x)), -(0 - ((r_unity.Width * y) + 2 * delta)));
+
+            //Reset the graphics object Transformations.
+            g.ResetTransform();
+        }
+
+        private Complex[] RhoAPlot;
+        private Complex[] ZPlot;
+        private Font MyFont;
+        private int Steps = -1;
+
+        public void SetSteps(int steps, Font font)
+        {
+            Steps = steps - 1;
+            RhoAPlot = new Complex[steps];
+            ZPlot = new Complex[steps];
+            if (Font == null)
+                Font = this.Font;
+            MyFont = font;
+        }
+
+        // compute complex impedance and store data for display by paint event 
+        public Complex Set_Z(Complex rhoA, int index)
+        {
+            if (index < 0 || index > Steps)
+            {
+                throw new ArgumentOutOfRangeException("index is less than 0 or greater than Steps");
+            }
+
+            RhoAPlot[index] = rhoA;
+            // Convert G into a complex impedance Zx
+            Complex Zo = new Complex(50, 0);
+            Complex Z = new Complex(0, 0);
+            // VNA.ComputeComplexImpedance(rhoA, Zo, ref Z);
+            ZPlot[index] = Z;
+
+            return Z;
+        }
+
+        private int offsetX = 20;
+        private int offsetY = 20;
+
+        // Plot a complex Impedance on the Smith Chart
+        private void PlotPoints(Graphics g)
+        {
+            int origin_unity_x = offsetX;
+            int origin_unity_y = offsetY;
+
+            // define the coordinates of the centre of the unity circle
+            int middle_unity_x = origin_unity_x + unity_circle_dia / 2;
+            int middle_unity_y = origin_unity_y + unity_circle_dia / 2;
+
+            // define the pixel size of dot to display, this will need to scale when display is zoomed
+            int dot = 6;
+
+            Brush dotColour;
+
+            // translate the coordinates to the centre of the unity circle
+            g.TranslateTransform(middle_unity_x, middle_unity_y);
+
+            for (int i = 0; i < RhoAPlot.Length; ++i)
+            {
+                Complex rhoA = RhoAPlot[i];
+                Debug.WriteLine("G.Magnitude = {0}  G.Phase = {1}", rhoA.Magnitude, rhoA.Phase * 180 / Math.PI);
+                Debug.WriteLine("G.Real = {0}  G.Imaginary = {1}", rhoA.Real, rhoA.Imaginary);
+
+                // draw a dot on the chart at the location of Z', scale for diameter of the dot :)
+                if (i == 0)
+                    dotColour = Brushes.Green;      // first dot is green so we can see starting frequency
+                else
+                    dotColour = Brushes.Black;
+
+                g.FillEllipse(dotColour, (int)(unity_circle_dia / 2 * rhoA.Real) - dot / 2, -(int)(unity_circle_dia / 2 * rhoA.Imaginary) - dot / 2, dot, dot);
+            }
+
+            //Reset the graphics object Transformations.
+            g.ResetTransform();
+        }
+
+
+        private void PlotCurve(Graphics g)
+        {
+            int origin_unity_x = offsetX;
+            int origin_unity_y = offsetY;
+
+            // define the coordinates of the centre of the unity circle
+            int middle_unity_x = origin_unity_x + unity_circle_dia / 2;
+            int middle_unity_y = origin_unity_y + unity_circle_dia / 2;
+
+            // translate the coordinates to the centre of the unity circle
+            g.TranslateTransform(middle_unity_x, middle_unity_y);
+
+            Point[] Points = new Point[RhoAPlot.Length];        // create a points array ready to drawing a curve
+
+            for (int i = 0; i < RhoAPlot.Length; ++i)
+            {
+                Points[i] = new Point((int)(unity_circle_dia / 2 * RhoAPlot[i].Real), -(int)(unity_circle_dia / 2 * RhoAPlot[i].Imaginary));
+            }
+
+            p.Width = 3;                // need a fatter pen here
+            g.DrawCurve(p, Points);     // draw a curve through the points
+
+            //Reset the graphics object Transformations.
+            g.ResetTransform();
+            // reset pen width
+            p.Width = 1;
+
+
+        }
+
+        public void clearSmithChart()
+        {
+            Graphics g = CreateGraphics();
+
+            Rectangle r = new Rectangle(0, 0, SmithPictureBox.Width, SmithPictureBox.Height);  // outline round chart
+            // fill chart rectangle with white
+            g.FillRectangle(Brushes.White, r);
+
+            g.Dispose();
+        }
+
+        private void SmithPictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            if (MyFont == null)
+                MyFont = this.Font;
+
+            // unity_circle_dia can be determined to be the min of width and height, so the circle is 'round'!
+            unity_circle_dia = Math.Min(SmithPictureBox.Height, SmithPictureBox.Width) - 40;
+
+            DrawSmithChart(g);
+
+            if (Steps != -1)
+            {
+                if ((Steps == 0))
+                {
+                    PlotPoints(g);
+                }
+                else
+                {
+                    PlotPoints(g);
+                    //    PlotCurve(g);
+                }
+            }
+        }
+
+        private void SmithPictureBox_Click(object sender, EventArgs e)
+        {
+
+            Complex x = new Complex(1, 2);
+
+            double xmag = (x.Real - 1) / (x.Real + 1);
+            double teta = ((x.Imaginary - 1) / (x.Imaginary + 1));
+
+            Complex Z = new Complex(xmag, teta);
+
+            SetSteps(1, MyFont);
+            Set_Z(Z, Steps);
+
+            Textboxadd("added point");
+
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.SelectionStart = textBox1.Text.Length;  //here we add the autoscroll
+            textBox1.ScrollToCaret();
+        }
+
+
+
+    }
+}
